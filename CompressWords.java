b458179d4dc248f0148546a6960479af653f14b3
RaiS4N <santosh984970@gmail.com>
2016-10-15 21:49:55 +0900

    Create CompressWords.java

---
+++ CompressWords.java
@@ -0,0 +1,48 @@
+
+public void class compressWord{
+  public static void main(String args[]){
+
+compress(int X[],int L,int Y[]){
+int i,p,n;
+int h;
+
+h=X[0];
+p=0;
+n=0;
+
+for(i=0;i<1,1)
+    {
+  if(x[]!=h)
+  {
+      if()
+      {
+      Y[p-2]="*";
+      Y[p-1]=n;
+      }
+      Y[p]=X[i];
+      h=X[i];
+      p=p+1;
+      n=1;
+  }else
+  {
+  if(n>=4)
+  {
+    if(i=L-1)
+    {
+     Y[p-2]="*";
+      Y[p-1]=n;
+    }
+  }else{
+    
+    p=p+1;
+  
+  
+  }
+  }
+
+
+
+    }//for
+    }
+}
+}
