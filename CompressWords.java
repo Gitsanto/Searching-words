b66173549df47565b84949ac218503b1f2b56a28
RaiS4N <santosh984970@gmail.com>
2016-10-16 22:27:46 +0900

    Update CompressWords.java

--- CompressWords.java
+++ CompressWords.java
@@ -1,49 +1,48 @@
 
-public void class compressWord{
+public class CompressLetter{
   public static void main(String args[]){
+  
 
-compress(int X[],int L,int Y[]){
-int i,p,n;
-int h;
-int X[]={a,b,b,c,c,c,d,d,d,d,e,e,e,e,e};
+String[] X	={"a","b","b","c","c","c","d","d","d","d","e","e","e","e","e"};
+int i,n = 0;
 int L=X.length;
-h=X[0];
-p=0;
-n=0;
+String h=X[0];
+String[] Y = {""};
+int p=0;
 
-for(i=0;i<1,1)
+
+for(i=0;i<4;i++)
     {
-  if(x[i]!=h)
+  if(X[i]!=h)
   {
       if(n>=4)
       {
-      Y[p-2]="*";
-      Y[p-1]=n;
+    	  Y[p-2]="*";
+          Y[p-1]= n;
       }
       Y[p]=X[i];
       h=X[i];
       p=p+1;
       n=1;
-  }else
-  {
+  }else{
   if(n>=4)
   {
-    if(i=L-1)
+    if(i==L-1)
     {
-     Y[p-2]="*";
+      Y[p-2]="*";
       Y[p-1]=n;
-    }
-  }else{
-    
-    p=p+1;
+     }else{
+      Y[p]=X[i];
+      p=p+1;
   
   
   }
   }
 
 
+  }
+}
+System.out.println(Y);
 
-    }//for
-    }
-}
-}
+
+}}
