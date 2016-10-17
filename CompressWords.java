a25bb50424e1ed91ca2d3ad51a4d340468ccf247
RaiS4N <santosh984970@gmail.com>
2016-10-17 14:57:16 +0900

    Update CompressWords.java

--- CompressWords.java
+++ CompressWords.java
@@ -1,49 +1,48 @@
 
-public class CompressLetter{
-  public static void main(String args[]){
-  
-
-String[] X	={"a","b","b","c","c","c","d","d","d","d","e","e","e","e","e"};
-int i,n = 0;
-int L=X.length;
-String h=X[0];
-String[] Y = {""};
-int p=0;
 
 
-for(i=0;i<4;i++)
+import java.util.ArrayList;
+
+public class CompressLetter {
+    public static void main(String[] args) {
+        String[] X = {"a","b","b","c","c","c","d","d","d","d","e","e","e","e","e"};
+        int i, n = 0;
+        int L = X.length;
+       ArrayList Y = new ArrayList();
+        String h = X[0];
+        int p = 0;
+        for(i = 0;i < L;i++)
     {
-  if(X[i]!=h)
-  {
-      if(n>=4)
-      {
-    	  Y[p-2]="*";
-          Y[p-1]= n;
-      }
-      Y[p]=X[i];
-      h=X[i];
-      p=p+1;
-      n=1;
-  }else{
-    n=n+1;
-  if(n>=4)
-  {
-    if(i==L-1)
-    {
-      Y[p-2]="*";
-      Y[p-1]=n;
-     }else{
-      Y[p]=X[i];
-      p=p+1;
-  
-  
-  }
-  }
-
-
-  }
+        if(X[i]!=h)
+        {
+            if(n>=4)
+            {
+                Y.add("*");
+                Y.add(Integer.toString(n));
+            }
+          
+          Y.add(X[i]);
+            h=X[i];
+            p=p+1;
+            n = 1;
+            
+        }
+        else{
+            n = n + 1;
+        if(n >= 4)
+        {
+          if(i==L-1)
+          {
+            Y.add("*");
+            Y.add(Integer.toString(n));
+          }
+        }
+          else{
+           Y.add(X[i]);
+            p=p+1;
+          }
+        }
+    }
+        System.out.println(Y);
+    }    
 }
-System.out.println(Y);
-
-
-}}
